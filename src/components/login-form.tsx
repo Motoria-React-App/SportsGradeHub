549c060a07af4079673ff70489f93d80bbae1571
Amrit <amritpal.alampur@gmail.com>
2026-01-26 19:19:10 +0100

    fix warnings

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -82,8 +82,7 @@ export function LoginForm({
         }
     }
 
-    const handleSocialLogin = (_provider: 'apple' | 'google') => {
-
+    const handleSocialLogin = () => {
         navigate(getDefaultRoute())
     }
 
@@ -117,7 +116,7 @@ export function LoginForm({
                                 <Button
                                     variant="outline"
                                     type="button"
-                                    onClick={() => handleSocialLogin('google')}
+                                    onClick={() => handleSocialLogin()}
                                     disabled={true}
                                 >
                                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
